090f67521c3a837a8c08a31f61b1440b92430e57
Arka Chakraborty <arkac720@gmail.com>
2023-05-26 11:51:04 +0530

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./compo/Navbar";
 
 import Home from "./pages/Home";
+import { About } from "./pages/About";
+import { Blog } from "./pages/Blog";
+import { Contact } from "./pages/Contact";
+import { Login } from "./pages/Login";
 
 function App() {
   return (
     <div>
+      
       <Navbar />
-
-      <Home />
+       <Routes>
+         <Route path="/" Component={Home}/>
+         <Route path="/about" Component={About}/>
+         <Route path="/blog" Component={Blog}/>
+         <Route path="/contact" Component={Contact}/>
+         <Route path="/login" Component={Login}/>
+       </Routes>
+     
     </div>
   );
 }
