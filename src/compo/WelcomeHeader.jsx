51141849e9f64e09af09ff0bb06409e8b7f776d4
Arka Chakraborty <arkac720@gmail.com>
2023-05-26 12:52:38 +0530

    css changed

--- src/compo/WelcomeHeader.jsx
+++ src/compo/WelcomeHeader.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const WelcomeHeader = () => {
   return (
-    <header className="bg-indigo-700 py-8 ">
+    <header className="bg-gradient-to-r from-indigo-500 ... ">
       <div className="container mx-auto text-center">
         <h1 className="text-4xl font-bold text-white">Welcome to BlogCom</h1>
         <p className="text-lg text-white mt-2">
